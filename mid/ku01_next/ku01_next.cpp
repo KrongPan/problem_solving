b2f583f66edadaac1dbdb745d774cb979a28f1a1
KrongPan <89672817+KrongPan@users.noreply.github.com>
2022-11-28 19:20:19 +0700

    save for ku01_next

--- mid/ku01_next/ku01_next.cpp
+++ mid/ku01_next/ku01_next.cpp
@@ -3,14 +3,18 @@ using namespace std;
 
 int main(){
   int N;
+  int object[N];
   int person[N];
   for (int i=0;i<N;i++) {
-    cin >> person[i];
+    person[N] = 0;
+    cin >> object[i];
   }
   while(true){
-    
     for (int i=0;i<N;i++) {
-      person[i] = person[person[i]];
+      
+    }
+    for (int i=0;i<N;i++) {
+      object[i] = object[object[i]];
     } 
   }
 }
