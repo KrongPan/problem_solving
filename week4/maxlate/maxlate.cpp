3f07cd913646243db6d5491f6f1e9e0413d75a9b
KrongPan <89672817+KrongPan@users.noreply.github.com>
2023-01-29 18:57:33 +0700

    idk

--- week4/maxlate/maxlate.cpp
+++ week4/maxlate/maxlate.cpp
@@ -1,35 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int maxt = 10000;
 int main()
 {
     int n, d, c;
-    int total = 0;
+    int tp = 0;
     int max = 0;
     cin >> n;
-    array<int,10000000> p = {0};
+    int  p[maxt+1];
+    for (int i = 0; i < maxt + 1; i++)
+    {
+        p[i] = 0;
+    }
     int td = 0;
-    for (int i = 1; i < n + 1; i++)
+    for (int i = 0; i < n; i++)
     {
         cin >> d >> c;
         p[d] = c;
     }
-    for (int i = 1; i < 10000000 + 1; i++)
+    for (int i = 1; i < maxt + 1; i++)
     {
         td += p[i];
-        if (p[i] != 0 && td - i - 10 > 0)
+        int total = 0;
+        if (td - i - 10 > 0)
         {
             // cout << '[' << i << ']';
-            if (total < (td - i - 10) * 1000)
+            total = (td - i - 10) * 1000;
+            // cout << total << '\n';
+            if (total > max)
             {
-                total = (td - i - 10) * 1000;
-                //cout << total << '\n';
-                if (total > max)
-                {
-                    max = total;
-                }
+                max = total;
             }
         }
     }
     cout << max;
+    return 0;
 }
